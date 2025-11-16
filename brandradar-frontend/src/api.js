7831fac96a0e3ea64995edc57ae443e1363c4aea
Yashbhalerao23 <yashbhalerao2304@gmail.com>
2025-11-16 14:13:01 +0530

    Create api.js

---
+++ brandradar-frontend/src/api.js
@@ -0,0 +1,9 @@
+// src/api.js (you can create this file)
+import axios from 'axios';
+
+const BASE_URL = "https://brand-radar.onrender.com/api";
+
+export const getBrands = async () => {
+    const response = await axios.get(`${BASE_URL}/brands/`);
+    return response.data;
+};
